97be9cbf05c39afe5ecd0f2235a127ecca69630b
Tomas Havlicek <thavlice@ibm.com>
2025-11-05 14:51:32 +0100

    chore: remove patternfly mentions

--- ui/src/config.js
+++ ui/src/config.js
@@ -1,5 +1,5 @@
 // for development purposes, you can change the backend url here
 // this URL is overriden in a container environment by the run-with-env.sh script
 window._env_ = {
-  API_URL: "https://sbomer-stage.pnc.engineering.redhat.com",
+  API_URL: "http://localhost:8080",
 };
